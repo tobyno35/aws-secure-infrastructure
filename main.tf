271b6eab50167a494728274cd4d71fc664e26586
toby orisadare <tobistic2013@gmail.om>
2026-02-13 11:39:07 -0500

    Add main.tf for AWS VPC configuration
    Adds a basic Terraform configuration for creating an AWS VPC.
        Includes provider definition and a VPC resource with a CIDR block and name tag.

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+      required_providers {
+        aws = {
+          source  = "hashicorp/aws"
+          version = "~> 5.0" # Using a slightly newer version based on other results
+        }
+      }
+    }
+    
+    provider "aws" {
+      region = "us-east-1"
+    }
+    
+    resource "aws_vpc" "main" {
+      cidr_block       = "10.0.0.0/16"
+      instance_tenancy = "default"
+    
+      tags = {
+        Name = "main-vpc"
+      }
+    }
+    
